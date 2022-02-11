6954d4a5bd3d119f8ba1ed6cd8ac298a0f92da99
mellowmorgan <morgan.waites@gmail.com>
2022-02-11 08:41:24 -0800

    add drawio component diagram add Header component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header'
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <KegControl/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>Welcome to the Keg Store</h1>
+  );
+}
+
+export default App;
